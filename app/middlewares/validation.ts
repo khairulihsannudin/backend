09e4ecee984ce555dad62f4c128e1fb077bff057
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-03-02 21:23:02 +0700

    fx((validation): fix name validation in user input middleware

--- app/middlewares/validation.ts
+++ app/middlewares/validation.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-import {body, validationResult} from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 export const validateUserInput = [
-  body('name').notEmpty().withMessage('Name is required'),
+  body('name').custom(value => /^[A-Za-z\s]+$/.test(value)).withMessage('Name is invalid'),
   body('email').isEmail().withMessage('Email is not valid'),
   body('phone').isMobilePhone('id-ID').withMessage('Phone number is not valid'),
   body('gender').isIn(['Male', 'Female']).withMessage('Gender must be either Male or Female'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+
   (req:Request, res:Response, next:NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
