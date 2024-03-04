91952341048548bb0f67a1a0dc3368d507691dad
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-03-04 22:56:53 +0700

    fix: add logic on numberOfParticipants register event
    looking at the length of participants array whenever it gets post or delete

--- app/routes/events.ts
+++ app/routes/events.ts
@@ -19,20 +19,22 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 //POST /events/register
-
 router.post('/register', authenticate, async (req: RequestWithUser, res: Response) => {
     try {
         const { _id } = req.body;
         const event = await Event.findById(_id);
 
         if (!event) return res.status(404).json({ error: 'Event not found' });
-        event.participants.push(req.user?.id);
         const user = await User.findById(req.user?.id);
-
+        
         if (!user) return res.status(404).json({ error: 'User not found' });
         if (user.events.includes(_id)) return res.status(400).json({ error: 'User already registered for this event' });
-
+        if((event.numberOfParticipants ?? 0) >= (event.maxParticipants ?? 0)) return res.status(400).json({ error: 'Event is full' });
+        
+        event.participants.push(req.user?.id);
+        event.numberOfParticipants = event.participants.length ?? 0;
         user.events.push(_id);
+        
         await user.save();
         await event.save();
         res.status(200).json(event);
@@ -44,16 +46,18 @@ router.post('/register', authenticate, async (req: RequestWithUser, res: Respons
 );
 
 //DELETE /events/unregister
-router.delete('/unregister', authenticate, async (req: RequestWithUser, res: Response) => {
+router.delete('/register', authenticate, async (req: RequestWithUser, res: Response) => {
     try {
         const { _id } = req.body;
         const event = await Event.findById(_id);
+        const user = await User.findById(req.user?.id);
+
         if (!event) return res.status(404).json({ error: 'Event not found' });
         if(!event.participants.includes(req.user?.id)) return res.status(400).json({ error: 'User is not registered for this event' });
-        event.participants = event.participants.filter((participantId: ObjectId) => participantId != req.user?.id);
-
-        const user = await User.findById(req.user?.id);
         if (!user) return res.status(404).json({ error: 'User not found' });
+
+        event.participants = event.participants.filter((participantId: ObjectId) => participantId != req.user?.id);
+        event.numberOfParticipants = event.participants.length ?? 0;
         user.events =  user.events.filter((eventId: ObjectId) => eventId != _id);
 
         await user.save();
