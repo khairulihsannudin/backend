fae6b1ac916c607f7ab06920cd7d13ba4f519fe6
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-03-08 14:24:29 +0700

    fix: tokenCache middleware is not returning the right refresh token
    parisng the data received from cache then choose the refresh token so that the jwt verify can verify the token
    
    adding req.user = user so that the route PUT /users/refresh-token can use it for assigning new access-token

--- app/middlewares/tokenCache.ts
+++ app/middlewares/tokenCache.ts
@@ -12,13 +12,16 @@ const tokenCache = (req: Request, res: Response, next: NextFunction) => {
     client.get(key, (err, data) => {
         if (err) throw err;
         if (data !== null) {
-            jwt.verify(data as string, process.env.JWT_SECRET as string, (err, user) => {
+            const refresh_token = JSON.parse(data as string).refresh_token;
+            if(typeof refresh_token !== 'string') return res.status(401).json({ error: 'Invalid token.' });
+            jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET as string, (err:any, user:any) => {
                 if (err) return res.status(401).json({ error: 'Invalid token.' });
                 res.set('X-Cache-Hit', 'true');
+                req.user = user;
                 next();
             });
-            next();
         } else {
+            res.set('X-Cache-Hit', 'false');
             res.status(401).json({ error: 'Invalid token.' });
         }
     });
