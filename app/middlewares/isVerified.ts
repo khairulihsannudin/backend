6fba583c3b58ea27941151e9408bb3da81f5cae3
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-03-11 21:10:25 +0700

    fix: isVerified middleware to use id to find the user verified section
    Before this the verified is accessed directly from the req.user.verified. Since it will not work because the req.user only contains id, so I fix it

--- app/middlewares/isVerified.ts
+++ app/middlewares/isVerified.ts
@@ -1,8 +1,11 @@
 import { Response, NextFunction } from "express";
 import { RequestWithUser } from "../interfaces/IUser";
+import User from "../models/user";
 
-const isVerified = (req: RequestWithUser, res: Response, next: NextFunction) => {
-    if (!req.user?.verified) return res.status(401).json({ error: 'User is not verified' });
+const isVerified =  async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const user = await User.findById(req.user?.id)
+    const verified = user?.verified;
+    if (!verified) return res.status(401).json({ error: 'User is not verified' });
     next();
 }
 
